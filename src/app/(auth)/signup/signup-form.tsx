196ad214505c3d592c0db83e56df0b499e12a489
takeshi0518 <y.takeshi0518@gmail.com>
2025-11-13 19:02:30 +0900

    feature: signupFormのバリデーション完了

--- src/app/(auth)/signup/signup-form.tsx
+++ src/app/(auth)/signup/signup-form.tsx
@@ -22,38 +22,78 @@ const signupSchema = z
       .min(1, 'パスワードは必須です')
       .min(8, 'パスワードは8文字以上である必要があります'),
 
-    comfirmPassword: z.string().min(1, 'パスワード確認は必須です'),
+    confirmPassword: z.string().min(1, 'パスワード確認は必須です'),
   })
-  .refine((data) => data.password === data.comfirmPassword, {
+  .refine((data) => data.password === data.confirmPassword, {
     message: 'パスワードが一致しません',
     path: ['confirmPassword'],
   });
 
+type SignupFormValue = z.infer<typeof signupSchema>;
+
 export default function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignupFormValue>({
+    resolver: zodResolver(signupSchema),
+    mode: 'onChange',
+  });
+
+  async function onSubmit(data: SignupFormValue) {
+    const formData = new FormData();
+    formData.append('email', data.email);
+    formData.append('password', data.password);
+
+    await signUp(formData);
+  }
+
   return (
-    <form action={signUp} className="space-y-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
       <div className="space-y-2">
         <Label htmlFor="email">メールアドレス</Label>
         <Input
           id="email"
-          name="email"
           type="email"
           placeholder="youremail@example.com"
+          {...register('email')}
         />
+        <div className="min-h-5">
+          {errors.email && (
+            <p className="text-sm text-red-500">{errors.email.message}</p>
+          )}
+        </div>
       </div>
 
       <div className="space-y-2">
         <Label htmlFor="password">パスワード</Label>
-        <Input id="password" name="password" type="password" />
+        <Input id="password" type="password" {...register('password')} />
+        <div className="min-h-5">
+          {errors.password && (
+            <p className="text-sm text-red-500">{errors.password.message}</p>
+          )}
+        </div>
       </div>
 
       <div className="space-y-2">
-        <Label htmlFor="password">パスワード確認</Label>
-        <Input id="password" name="password" type="password" />
+        <Label htmlFor="confirmPassword">パスワード確認</Label>
+        <Input
+          id="confirmPassword"
+          type="password"
+          {...register('confirmPassword')}
+        />
+        <div className="min-h-5">
+          {errors.confirmPassword && (
+            <p className="text-sm text-red-500">
+              {errors.confirmPassword.message}
+            </p>
+          )}
+        </div>
       </div>
 
       <Button type="submit" className="w-full cursor-pointer">
-        登録
+        {isSubmitting ? '登録中...' : '登録'}
       </Button>
     </form>
   );
