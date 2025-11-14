dfe396a9ad2d94f94318f3a100da341c7738d85b
takeshi0518 <y.takeshi0518@gmail.com>
2025-11-14 21:32:16 +0900

    feature: メール確認画面を作成

---
+++ src/app/(auth)/confirm-email/page.tsx
@@ -0,0 +1,9 @@
+export default function ConfirmEmailPage() {
+  return (
+    <div>
+      <h1>メールを確認してください</h1>
+      <p>登録したメールアドレスに確認メールを送信しました。</p>
+      <p>メール内のリンクをクリックして登録を完了してください。</p>
+    </div>
+  );
+}
