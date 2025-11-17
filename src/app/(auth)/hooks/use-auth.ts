6951a6b0f50b269d432caba44ae8db21aa3239c5
takeshi0518 <y.takeshi0518@gmail.com>
2025-11-17 23:33:21 +0900

    feature: github OAuth認証時にアカウントを選択するオプションを追加

--- src/app/(auth)/hooks/use-auth.ts
+++ src/app/(auth)/hooks/use-auth.ts
@@ -86,6 +86,9 @@ export function useAuth() {
         provider: 'github',
         options: {
           redirectTo: `${window.location.origin}/auth/callback`,
+          queryParams: {
+            prompt: 'select_account',
+          },
         },
       });
 
