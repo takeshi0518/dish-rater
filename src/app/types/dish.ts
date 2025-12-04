c41f8f7a0c262014a52334f953518774a5196fd1
takeshi0518 <y.takeshi0518@gmail.com>
2025-12-04 17:12:11 +0900

    feature: types/dish.tsxにProfile型を追加

--- src/app/types/dish.ts
+++ src/app/types/dish.ts
@@ -12,3 +12,12 @@ export type Dish = {
   created_at: string;
   updated_at: string;
 };
+
+export type Profile = {
+  id: string;
+  username: string;
+  avatar_url: string | null;
+  bio: string | null;
+  created_at: string;
+  updated_at: string;
+};
